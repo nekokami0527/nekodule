86a24fc1e7eff72ddf419abfc5398241566059d6
root <xuechen2000410@126.com>
2019-11-24 03:17:11 +0800

    Add Download,tcpproxy Modules

---
+++ downloadfile.py
@@ -0,0 +1,14 @@
+import requests
+
+def run(**args):
+	url = args['url']
+	filename = url.split("/")[-1]
+	path = "c:\\" if not args['path'] else args['path']
+	r=requests.get(url).content
+	with open(path+'/'+str(filename),'wb') as f:
+		print('[*] Downloading File %s'%filename)
+		f.write(r)
+		f.close()
+
+if __name__ == '__main__':
+	run(url="https://img-blog.csdnimg.cn/20190927151132530.png",path="./")
